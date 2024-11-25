ba5f65bdeaed95c106078fd13d553c2303266890
Anjaszz <anjasgans841@gmail.com>
2024-11-25 19:07:55 +0700

    update 3

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,7 +9,7 @@ import LogoNic from '../assets/images/logo-nic.webp'
 const SeminarInfo = () => {
   return (
 <>
-<section className="bg-white dark:bg-gray-900 h-[45rem] pt-20">
+<section className="bg-white dark:bg-gray-900 h-[45rem] pt-0 lg:pt-20">
   <div className="grid max-w-screen-xl px-4  pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
     <div className="mr-auto lg:ml-14 place-self-center lg:col-span-7">
       <h1 className="max-w-2xl mb-4 lg:mb-8 text-4xl font-extrabold leading-6 tracking-tight md:text-5xl xl:text-6xl dark:text-white">
