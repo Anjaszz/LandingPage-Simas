4ee75a604ef5bdd0c5215a665654d66f33c51870
Anjaszz <anjasgans841@gmail.com>
2024-11-27 19:02:02 +0700

    update heading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,8 +11,8 @@ const SeminarInfo = () => {
 <>
 <section className="bg-white dark:bg-gray-900 h-[45rem] pt-0 lg:pt-20">
   <div className="grid max-w-screen-xl px-4  pt-20 pb-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12 ">
-    <div className="mr-auto lg:ml-14 place-self-center lg:col-span-7">
-      <h1 className="max-w-2xl mb-4 lg:mb-8 text-4xl font-extrabold leading-6 tracking-tight md:text-5xl xl:text-6xl dark:text-white">
+    <div className="mr-auto lg:ml-14 place-self-center lg:col-span-7 mt-10">
+      <h1 className="max-w-2xl mb-4 lg:mb-8 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
       Kelola Seminar Anda dengan Mudah
       </h1>
       <p className="max-w-2xl mb-6 font-medium font-sans text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
